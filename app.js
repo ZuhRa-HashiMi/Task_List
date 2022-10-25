323f139d35749eef44b967469a38ffc99f64b7d2
Zuhra Hashimi <zuhra.tactaz@yahoo.com>
2022-10-25 17:48:43 +0430

    add all Event listeners and Add task function

--- app.js
+++ app.js
@@ -4,3 +4,25 @@ const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
+
+// Load All Event Listnrs
+
+loadEventListeners();
+
+// Load All Event Listeners
+
+function loadEventListeners() {
+    // Add task event
+    form.addEventListener('submit', addTask);
+}
+
+// Add Task
+
+function addTask(e) {
+    if (taskInput.value === '') {
+        alert('add a task');
+    }
+
+    e.preventDefault();
+
+}
