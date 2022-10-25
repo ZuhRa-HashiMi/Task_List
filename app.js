b14ba0fef19be7335fa4d9907a6b106bc30df48f
Zuhra Hashimi <zuhra.tactaz@yahoo.com>
2022-10-25 17:23:38 +0430

    Define UI vars

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 // Define UI vars 
 const form = document.querySelector('#task-form');
-const 
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
